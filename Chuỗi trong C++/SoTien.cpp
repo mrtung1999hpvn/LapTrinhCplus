080f797db93b607d8d541a10bfe646bc9acac828
ThanhTung <63633930+mrtung1999hpvn@users.noreply.github.com>
2020-07-27 15:31:51 +0700

    Update SoTien.cpp

--- Chuỗi trong C++/SoTien.cpp
+++ Chuỗi trong C++/SoTien.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 int main()
 {
-    string a="500000";
+    string a="5000000000";
     string kq="d";
     int t=0;
     for(int i=a.length();i>=0;i--) // Sắp xếp nổi bọt 
     {
-        t++;
+        
         kq+=a[i];
         if(i==0){break;}
-        if(t==4 || t==7 || t==10 &&t!=0){kq+=",";}
+        if(t%3==0 && t!=0){kq+=",";}
+        t++;
     }
     reverse(kq.begin(), kq.end()); 
     cout<<kq;
